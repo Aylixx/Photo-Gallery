5ea588c6b18818a2289becd7580b59d86bba60c5
Colton Aylix <cxaylix@gmail.com>
2023-01-08 00:44:12 -0700

    Ready for online

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,7 +1,17 @@
-const header = document.querySelector(".header");
+const header = document.querySelector("header");
+const sky = document.querySelector(".mobile-header-image");
+const trees = document.querySelector(".mobile-header-image-2");
+const signature = document.querySelector(".logo");
 
 window.addEventListener("scroll", () => {
-    let speed = window.scrollY / -1.5;
-    if (speed != 0)
-        header.style.backgroundPosition = "0" + speed + "px";
+    let headerSpeed = window.scrollY / -2.5;
+    let skySpeed = window.scrollY / -8;
+    let treeSpeed = window.scrollY / -2;
+
+    if (window.innerWidth >= 768 && headerSpeed != 0) {
+        header.style.backgroundPosition = "0" + headerSpeed + "px";
+    } else if (skySpeed != 0 && treeSpeed != 0) {
+        sky.style.backgroundPosition = "0" + skySpeed + "px";
+        trees.style.backgroundPosition = "0" + treeSpeed + "px";
+    }
 });
